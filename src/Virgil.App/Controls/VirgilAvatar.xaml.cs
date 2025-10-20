a01307fdece8fa8cca02cada5c928c166429974f
thoms <bassetthomas@hotmail.fr>
2025-10-20 01:45:15 -0700

    Add VirgilAvatar.xaml.cs code-behind

---
+++ src/Virgil.App/Controls/VirgilAvatar.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Virgil.App.Controls
+{
+    public partial class VirgilAvatar : UserControl
+    {
+        public VirgilAvatar()
+        {
+            InitializeComponent();
+        }
+    }
+}
