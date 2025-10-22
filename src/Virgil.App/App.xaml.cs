d0a739539556c84618d5b46618ec796f10635b8f
thoms <bassetthomas@hotmail.fr>
2025-10-22 14:37:14 +0200

    Update App.xaml.cs

--- src/Virgil.App/App.xaml.cs
+++ src/Virgil.App/App.xaml.cs
@@ -1,22 +1,29 @@
 #nullable enable
+using System;
 using System.Windows;
-using Serilog.Events;
-using Virgil.Core.Services;
 
 namespace Virgil.App
 {
     public partial class App : Application
     {
+        private Tray.TrayIconService? _tray;
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            LoggingService.Init(LogEventLevel.Information);
-            var _ = new ConfigService(); // force création dossiers + charge config
+
+            // Crée la fenêtre principale
+            var win = new MainWindow();
+            win.Show();
+
+            // Tray (menu: Ouvrir, Maintenance rapide, MAJ apps/jeux, Quitter)
+            _tray = new Tray.TrayIconService(win);
+            _tray.Initialize();
         }
 
         protected override void OnExit(ExitEventArgs e)
         {
-            Serilog.Log.CloseAndFlush();
+            try { _tray?.Dispose(); } catch { /* ignore */ }
             base.OnExit(e);
         }
     }
