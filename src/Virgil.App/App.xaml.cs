a504c7f9758f74d0841f8710ce25214d675ba3df
thoms <bassetthomas@hotmail.fr>
2025-10-22 09:49:18 +0200

    Update App.xaml.cs

--- src/Virgil.App/App.xaml.cs
+++ src/Virgil.App/App.xaml.cs
@@ -1,8 +1,23 @@
+#nullable enable
 using System.Windows;
+using Serilog.Events;
+using Virgil.Core.Services;
 
 namespace Virgil.App
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            LoggingService.Init(LogEventLevel.Information);
+            var _ = new ConfigService(); // force création dossiers + charge config
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Serilog.Log.CloseAndFlush();
+            base.OnExit(e);
+        }
     }
 }
