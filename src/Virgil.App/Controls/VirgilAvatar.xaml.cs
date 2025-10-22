fdfc332167241476e5ea0640c848abc55569be56
thoms <bassetthomas@hotmail.fr>
2025-10-22 15:15:09 +0200

    Update VirgilAvatar.xaml.cs

--- src/Virgil.App/Controls/VirgilAvatar.xaml.cs
+++ src/Virgil.App/Controls/VirgilAvatar.xaml.cs
@@ -1,11 +1,14 @@
 #nullable enable
+using System.Windows.Controls;
+
 namespace Virgil.App.Controls
 {
-    public partial class VirgilAvatar : System.Windows.Controls.UserControl
+    public partial class VirgilAvatar : UserControl
     {
         public VirgilAvatar()
         {
             InitializeComponent();
+            // L’avatar attend un DataContext = VirgilAvatarViewModel (déjà prêt)
         }
     }
 }
