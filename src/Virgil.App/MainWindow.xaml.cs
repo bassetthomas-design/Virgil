276f2518435321c5d126b897780f1b79ff8d4427
thoms <bassetthomas@hotmail.fr>
2025-10-23 09:57:40 +0200

    Update MainWindow.xaml.cs

--- src/Virgil.App/MainWindow.xaml.cs
+++ src/Virgil.App/MainWindow.xaml.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
+using System.Windows.Media.Brush _myBrush; // (optionnel)
 using System.Windows.Threading;
 
 using Serilog.Events;
