1f0e381d832cd77c293310374634676145b088f2
thoms <bassetthomas@hotmail.fr>
2025-10-23 19:23:40 +0200

    Update VirgilAvatarViewModel.cs

--- src/Virgil.App/Controls/VirgilAvatarViewModel.cs
+++ src/Virgil.App/Controls/VirgilAvatarViewModel.cs
@@ -1,7 +1,6 @@
 #nullable enable
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-// Alias WPF pour éviter tout conflit avec System.Drawing.*
 using Media = System.Windows.Media;
 
 namespace Virgil.App.Controls
@@ -12,18 +11,16 @@ namespace Virgil.App.Controls
         private void OnPropertyChanged([CallerMemberName] string? n = null)
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(n));
 
-        // Couleurs / glow
         public Media.Brush FaceBrush { get => _faceBrush; set { _faceBrush = value; OnPropertyChanged(); } }
         public Media.Brush EyeBrush  { get => _eyeBrush;  set { _eyeBrush = value;  OnPropertyChanged(); } }
         public Media.Color GlowColor { get => _glowColor; set { _glowColor = value; OnPropertyChanged(); } }
         public double GlowOpacity    { get => _glowOpacity; set { _glowOpacity = value; OnPropertyChanged(); } }
 
-        private Media.Brush _faceBrush = new Media.SolidColorBrush(Media.Color.FromRgb(98, 125, 78)); // vert logo
+        private Media.Brush _faceBrush = new Media.SolidColorBrush(Media.Color.FromRgb(98, 125, 78));
         private Media.Brush _eyeBrush  = Media.Brushes.White;
         private Media.Color _glowColor = Media.Color.FromRgb(39, 215, 255);
         private double _glowOpacity = 0.40;
 
-        // Géométrie yeux amande
         public double EyeScale { get => _eyeScale; set { _eyeScale = value; OnPropertyChanged(); } }
         public double EyeTilt { get => _eyeTilt; set { _eyeTilt = value; OnPropertyChanged(); } }
         public double EyeSeparation { get => _eyeSeparation; set { _eyeSeparation = value; OnPropertyChanged(); } }
@@ -34,7 +31,6 @@ namespace Virgil.App.Controls
         private double _eyeSeparation = 34;
         private double _eyeY = 0;
 
-        // Drapeaux d’éléments spéciaux
         public bool UseRoundEyes { get => _round; set { _round = value; OnPropertyChanged(); } }
         public bool ShowTear     { get => _tear;  set { _tear = value;  OnPropertyChanged(); } }
         public bool ShowHearts   { get => _hearts;set { _hearts = value;OnPropertyChanged(); } }
@@ -49,9 +45,8 @@ namespace Virgil.App.Controls
         {
             CurrentMood = (mood ?? "neutral").ToLowerInvariant();
 
-            // Réinitialise addons + couleurs par défaut
             UseRoundEyes = ShowTear = ShowHearts = ShowCat = ShowDevil = false;
-            FaceBrush = new Media.SolidColorBrush(Media.Color.FromRgb(98, 125, 78)); // vert par défaut
+            FaceBrush = new Media.SolidColorBrush(Media.Color.FromRgb(98, 125, 78));
             EyeBrush  = Media.Brushes.White;
 
             switch (CurrentMood)
@@ -60,45 +55,36 @@ namespace Virgil.App.Controls
                     EyeScale = 1.10; EyeTilt = 6; EyeSeparation = 36; EyeY = 2;
                     GlowColor = Media.Color.FromRgb(80, 220, 120); GlowOpacity = 0.5;
                     break;
-
                 case "proud":
                     EyeScale = 1.05; EyeTilt = 10; EyeSeparation = 35; EyeY = 0;
                     GlowColor = Media.Color.FromRgb(39, 215, 255); GlowOpacity = 0.55;
                     break;
-
                 case "vigilant":
                     EyeScale = 0.95; EyeTilt = 18; EyeSeparation = 36; EyeY = -1;
                     GlowColor = Media.Color.FromRgb(255, 210, 60); GlowOpacity = 0.6;
                     break;
-
                 case "alert":
                     EyeScale = 0.92; EyeTilt = 22; EyeSeparation = 36; EyeY = -2;
                     GlowColor = Media.Color.FromRgb(255, 80, 80);  GlowOpacity = 0.7;
                     break;
-
                 case "sleepy":
                     UseRoundEyes = true; GlowColor = Media.Color.FromRgb(120, 160, 255); GlowOpacity = 0.45;
                     break;
-
                 case "sad":
                     UseRoundEyes = true; ShowTear = true; GlowColor = Media.Color.FromRgb(120, 160, 255); GlowOpacity = 0.55;
                     break;
-
                 case "love":
                     UseRoundEyes = true; ShowHearts = true; GlowColor = Media.Color.FromRgb(255, 120, 200); GlowOpacity = 0.7;
                     break;
-
                 case "cat":
                     UseRoundEyes = true; ShowCat = true; GlowColor = Media.Color.FromRgb(255, 200, 120); GlowOpacity = 0.65;
                     break;
-
                 case "devil":
                     EyeScale = 0.95; EyeTilt = 20; EyeSeparation = 34; EyeY = -1;
                     ShowDevil = true; GlowColor = Media.Color.FromRgb(255, 80, 80); GlowOpacity = 0.8;
-                    FaceBrush = new Media.SolidColorBrush(Media.Color.FromRgb(190, 40, 40)); // face rouge
+                    FaceBrush = new Media.SolidColorBrush(Media.Color.FromRgb(190, 40, 40));
                     break;
-
-                default: // neutral
+                default:
                     EyeScale = 1.0; EyeTilt = 12; EyeSeparation = 34; EyeY = 0;
                     GlowColor = Media.Color.FromRgb(39, 215, 255); GlowOpacity = 0.40;
                     break;
