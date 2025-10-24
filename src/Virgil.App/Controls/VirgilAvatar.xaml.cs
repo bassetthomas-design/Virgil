299a1a37ec9c8a7549ab6157b787519c9a4a4706
thoms <bassetthomas@hotmail.fr>
2025-10-24 19:09:48 +0200

    Update VirgilAvatar.xaml.cs

--- src/Virgil.App/Controls/VirgilAvatar.xaml.cs
+++ src/Virgil.App/Controls/VirgilAvatar.xaml.cs
@@ -1,6 +1,5 @@
 #nullable enable
-using System.Windows.Controls;     // WPF UserControl
-using System.Windows.Media;
+using System.Windows.Controls;
 
 namespace Virgil.App.Controls
 {
@@ -14,11 +13,6 @@ namespace Virgil.App.Controls
             DataContext = _vm;
         }
 
-        // API utilisé par MainWindow : change l’humeur
         public void SetMood(string mood) => _vm.SetMood(mood);
-
-        // Optionnel : couleurs custom
-        public void SetBodyColor(Color c) => _vm.BodyBrush = new SolidColorBrush(c);
-        public void SetEyeColor(Color c)  => _vm.EyeBrush  = new SolidColorBrush(c);
     }
 }
