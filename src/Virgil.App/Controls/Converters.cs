c76e27ef4394c01da495f8d3b2e3a26f16b151b9
thoms <bassetthomas@hotmail.fr>
2025-10-24 19:08:34 +0200

    Update Converters.cs

--- src/Virgil.App/Controls/Converters.cs
+++ src/Virgil.App/Controls/Converters.cs
@@ -2,31 +2,29 @@
 using System;
 using System.Globalization;
 using System.Windows;
-using System.Windows.Data;        // Binding
-using System.Windows.Media;       // Color/Brush
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace Virgil.App.Controls
 {
-    // string/hex -> Brush (SolidColorBrush)
+    // Convertit Color ou string vers Brush
     public sealed class ColorToBrushConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is Brush b) return b;
-
             if (value is Color c) return new SolidColorBrush(c);
 
             if (value is string s && !string.IsNullOrWhiteSpace(s))
             {
                 try
                 {
-                    var cc = new ColorConverter();              // WPF ColorConverter
+                    var cc = new ColorConverter();
                     var col = (Color)cc.ConvertFromString(s)!;
                     return new SolidColorBrush(col);
                 }
                 catch { }
             }
-
             return Brushes.Transparent;
         }
 
@@ -34,24 +32,21 @@ namespace Virgil.App.Controls
             => throw new NotSupportedException();
     }
 
-    // double (séparation des yeux) -> Thickness (marges opposées)
+    // Double -> Thickness (pour espacement des yeux)
     public sealed class EyeSeparationToMarginConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             double sep = 0;
             if (value is double d) sep = d;
-            else if (value is float f) sep = f;
 
-            // param = "Left" ou "Right"
-            var side = parameter as string;
-            if (string.Equals(side, "Left", StringComparison.OrdinalIgnoreCase))
-                return new Thickness(-sep, 0, sep, 0);
-
-            if (string.Equals(side, "Right", StringComparison.OrdinalIgnoreCase))
-                return new Thickness(sep, 0, -sep, 0);
-
-            return new Thickness(0);
+            var side = (parameter as string)?.ToLowerInvariant();
+            return side switch
+            {
+                "left" => new Thickness(-sep, 0, sep, 0),
+                "right" => new Thickness(sep, 0, -sep, 0),
+                _ => new Thickness(0)
+            };
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
