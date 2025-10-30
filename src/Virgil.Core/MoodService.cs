ce9395942ef88e1c2da2b1cac529023f570d87d5
thoms <bassetthomas@hotmail.fr>
2025-10-30 17:50:56 +0100

    Update MoodService.cs

--- src/Virgil.Core/MoodService.cs
+++ src/Virgil.Core/MoodService.cs
@@ -1,50 +1,51 @@
-*** Begin Patch
-*** Update File: src/Virgil.Core/MoodService.cs
-@@
--    public enum Mood
-+    public enum AvatarMood
-     {
-         Neutral,
-         Vigilant,
-         Alert,
-         Resting,
-         Proud
-     }
-@@
--        private Mood _currentMood = Mood.Neutral;
-+        private AvatarMood _currentMood = AvatarMood.Neutral;
-@@
--        public Mood CurrentMood
-+        public AvatarMood CurrentMood
-         {
-             get => _currentMood;
-             set
-             {
-                 if (_currentMood != value)
-                 {
-                     _currentMood = value;
-                     MoodChanged?.Invoke(this, EventArgs.Empty);
-                 }
-             }
-         }
-@@
--        public string GetMoodColor()
-+        public string GetMoodColor()
-         {
-             return _currentMood switch
-             {
--                Mood.Neutral  => "#007ACC",
--                Mood.Vigilant => "#FFA500",
--                Mood.Alert    => "#FF4500",
--                Mood.Resting  => "#00CED1",
--                Mood.Proud    => "#32CD32",
--                _             => "#007ACC"
-+                AvatarMood.Neutral  => "#007ACC",
-+                AvatarMood.Vigilant => "#FFA500",
-+                AvatarMood.Alert    => "#FF4500",
-+                AvatarMood.Resting  => "#00CED1",
-+                AvatarMood.Proud    => "#32CD32",
-+                _                  => "#007ACC"
-             };
-         }
-*** End Patch
+using System;
+
+namespace Virgil.Core
+{
+    /// <summary>
+    /// Fournit l’humeur courante et une couleur associée.
+    /// Utilise l’énumération AvatarMood définie dans Contracts.cs.
+    /// </summary>
+    public class MoodService
+    {
+        private AvatarMood _currentMood = AvatarMood.Neutral;
+
+        /// <summary>
+        /// Se déclenche quand l’humeur change.
+        /// </summary>
+        public event EventHandler? MoodChanged;
+
+        /// <summary>
+        /// Obtient ou définit l’humeur courante.
+        /// Déclenche l’événement MoodChanged en cas de modification.
+        /// </summary>
+        public AvatarMood CurrentMood
+        {
+            get => _currentMood;
+            set
+            {
+                if (_currentMood != value)
+                {
+                    _currentMood = value;
+                    MoodChanged?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retourne un code couleur hexadécimal correspondant à l’humeur courante.
+        /// </summary>
+        public string GetMoodColor()
+        {
+            return _currentMood switch
+            {
+                AvatarMood.Neutral  => "#007ACC",
+                AvatarMood.Vigilant => "#FFA500",
+                AvatarMood.Alert    => "#FF4500",
+                AvatarMood.Resting  => "#00CED1",
+                AvatarMood.Proud    => "#32CD32",
+                _ => "#007ACC"
+            };
+        }
+    }
+}
