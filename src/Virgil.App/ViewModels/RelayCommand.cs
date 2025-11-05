0dc9f1fc8200ccec75638b90bccc42be720c1c49
thoms <bassetthomas@hotmail.fr>
2025-11-05 22:26:07 +0100

    Update RelayCommand.cs

--- src/Virgil.App/ViewModels/RelayCommand.cs
+++ src/Virgil.App/ViewModels/RelayCommand.cs
@@ -3,40 +3,25 @@ using System.Windows.Input;
 
 namespace Virgil.App.ViewModels
 {
-    /// <summary>
-    /// Implémentation simple d'un ICommand (style MVVM).
-    /// </summary>
-    internal sealed class RelayCommand : ICommand
+    public sealed class RelayCommand : ICommand
     {
         private readonly Action<object?> _execute;
-        private readonly Func<object?, bool>? _canExecute;
+        private readonly Predicate<object?>? _canExecute;
 
-        public RelayCommand(Action execute)
-        {
-            _execute = _ => execute();
-        }
-
-        public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+        public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
-        }
-
-        public RelayCommand(Action execute, Func<bool> canExecute)
-        {
-            if (execute == null) throw new ArgumentNullException(nameof(execute));
-            if (canExecute == null) throw new ArgumentNullException(nameof(canExecute));
-
-            _execute = _ => execute();
-            _canExecute = _ => canExecute();
         }
 
         public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
 
         public void Execute(object? parameter) => _execute(parameter);
 
-        public event EventHandler? CanExecuteChanged;
-
-        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
     }
 }
