be7903df6ade14808face0aaacc86eb391a80f30
thoms <bassetthomas@hotmail.fr>
2025-11-07 19:40:11 +0100

    Fix: simplifier AvatarControl pour supprimer dépendances à Virgil.Core/Mood (compilation WPF)

--- src/Virgil.App/Controls/AvatarControl.xaml.cs
+++ src/Virgil.App/Controls/AvatarControl.xaml.cs
@@ -1,25 +1,11 @@
 using System.Windows.Controls;
-using Virgil.Core;
 
-namespace Virgil.App.Controls
+namespace Virgil.App.Controls;
+
+public partial class AvatarControl : UserControl
 {
-    public partial class AvatarControl : UserControl
+    public AvatarControl()
     {
-        public AvatarControl()
-        {
-            InitializeComponent();
-        }
-
-        public void SetMood(Mood mood)
-        {
-            // TODO: switch mood -> sprite (happy, focused, warn, alert, sleepy, proud, tired, etc.)
-            // Exemple:
-            // switch (mood)
-            // {
-            //     case Mood.Neutral:   Sprite.Source = ...; break;
-            //     case Mood.Focused:   Sprite.Source = ...; break;
-            //     // ...
-            // }
-        }
+        InitializeComponent();
     }
 }
