bc1c5b015972e12737e0db5924ccffc86c8cca25
thoms <bassetthomas@hotmail.fr>
2025-11-08 19:17:12 +0100

    merge-fix: unify ISystemActionsService with main and add Rambo methods

--- src/Virgil.App/Services/ISystemActionsService.cs
+++ src/Virgil.App/Services/ISystemActionsService.cs
@@ -4,9 +4,12 @@ namespace Virgil.App.Services;
 
 public interface ISystemActionsService
 {
+    // présents sur main
     Task<int> WsResetAsync();
     Task<int> RebuildExplorerCachesAsync();
     Task<int> EmptyRecycleBinAsync();
+
+    // ajout Rambo
     Task<int> RestartExplorerAsync();
     Task<int> RebuildExplorerCachesAndRestartAsync();
 }
