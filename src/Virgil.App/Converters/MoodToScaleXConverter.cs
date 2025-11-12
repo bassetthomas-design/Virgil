38bd52a508b933b7c632bd16e21d02b4ac44e843
thoms <bassetthomas@hotmail.fr>
2025-11-12 21:13:58 +0100

    feat(converters): add individual Mood converters for XAML discovery

---
+++ src/Virgil.App/Converters/MoodToScaleXConverter.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Virgil.App.Converters
+{
+    public class MoodToScaleXConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var key = (value?.ToString() ?? string.Empty).ToLowerInvariant();
+            return key switch { "happy" => 1.2, "tired" => 0.95, "angry" => 1.05, "focused" => 1.0, _ => 1.0 };
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotSupportedException();
+    }
+}
