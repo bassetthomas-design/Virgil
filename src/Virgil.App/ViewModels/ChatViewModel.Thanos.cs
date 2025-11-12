468767d684220d8980eb99ba511e91bec13c11f6
thoms <bassetthomas@hotmail.fr>
2025-11-12 20:07:17 +0100

    fix(thanos): temporarily no-op SnapAll to avoid namespace/property conflicts; will rewire

--- src/Virgil.App/ViewModels/ChatViewModel.Thanos.cs
+++ src/Virgil.App/ViewModels/ChatViewModel.Thanos.cs
@@ -2,6 +2,6 @@ namespace Virgil.App.ViewModels
 {
     public partial class ChatViewModel
     {
-        public void SnapAll(){ Chat?.ClearAll(); }
+        public void SnapAll(){ /* TODO: call chatService.ClearAll() once exposed here */ }
     }
 }
