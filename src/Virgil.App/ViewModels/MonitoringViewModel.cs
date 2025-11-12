bd7417fd7a37ba61739c8f0aaaee8a83ebe6c52e
thoms <bassetthomas@hotmail.fr>
2025-11-12 21:53:15 +0100

    feat(mood): implement INotifyPropertyChanged for CurrentMood

--- src/Virgil.App/ViewModels/MonitoringViewModel.cs
+++ src/Virgil.App/ViewModels/MonitoringViewModel.cs
@@ -6,15 +6,14 @@ namespace Virgil.App.ViewModels
 {
     public partial class MonitoringViewModel : INotifyPropertyChanged
     {
-        private Mood _currentMood = default(Mood);
+        private Mood _currentMood = default;
         public Mood CurrentMood
         {
             get => _currentMood;
-            set { if (_currentMood.Equals(value)) return; _currentMood = value; OnPropertyChanged(); }
+            set { if (Equals(_currentMood, value)) return; _currentMood = value; OnPropertyChanged(); }
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string? name = null)
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        private void OnPropertyChanged([CallerMemberName] string? n = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(n));
     }
 }
