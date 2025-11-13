e25a7371267d5cc4b39d1f786a8eac45bafc72f0
thoms <bassetthomas@hotmail.fr>
2025-11-13 03:56:34 -0800

    Add ActionButton_Click handler to ActionsPanel and support IActionInvoker

--- src/Virgil.App/Views/ActionsPanel.xaml.cs
+++ src/Virgil.App/Views/ActionsPanel.xaml.cs
@@ -1,9 +1,25 @@
+using System.Windows;
 using System.Windows.Controls;
+uusing Virgil.App.Interfaces;
 
 namespace Virgil.App.Views
 {
     public partial class ActionsPanel : UserControl
     {
-        public ActionsPanel() { InitializeComponent(); }
+        public ActionsPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void ActionButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.Tag is string actionId)
+            {
+                if (this.DataContext is IActionInvoker invoker)
+                {
+                    invoker.InvokeAction(actionId);
+                }
+            }
+        }
     }
 }
