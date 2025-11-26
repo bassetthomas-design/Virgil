e4a6a478e6d71031803d4476ee4ca9af29601138
thoms <bassetthomas@hotmail.fr>
2025-11-26 21:02:21 +0100

    Simplify DashboardViewModel dependencies (SystemActionsService + VirgilNarrationService only)

--- src/Virgil.App/ViewModels/DashboardViewModel.cs
+++ src/Virgil.App/ViewModels/DashboardViewModel.cs
@@ -2,7 +2,6 @@ using System;
 using System.Windows.Input;
 using Virgil.App.Core;
 using Virgil.App.Services;
-using Virgil.Services;
 using Virgil.Services.Narration;
 
 namespace Virgil.App.ViewModels
@@ -11,37 +10,25 @@ namespace Virgil.App.ViewModels
     {
         private readonly SystemActionsService _systemActions;
         private readonly VirgilNarrationService _narration;
-        private readonly ChatService _chat;
 
-        public DashboardViewModel(
-            SystemActionsService systemActions,
-            VirgilNarrationService narration,
-            ChatService chat)
+        public DashboardViewModel(SystemActionsService systemActions, VirgilNarrationService narration)
         {
             _systemActions = systemActions ?? throw new ArgumentNullException(nameof(systemActions));
-            _narration = narration ?? throw new ArgumentNullException(nameof(narration));
-            _chat = chat ?? throw new ArgumentNullException(nameof(chat));
+            _narration     = narration     ?? throw new ArgumentNullException(nameof(narration));
 
-            RunMaintenanceCmd = new RelayCommand(_ => RunMaintenance());
-            SmartCleanupCmd = new RelayCommand(_ => RunSmartCleanup());
-            CleanBrowsersCmd = new RelayCommand(_ => RunBrowsersCleanup());
-            UpdateAllCmd = new RelayCommand(_ => RunUpdateAll());
-            RunDefenderScanCmd = new RelayCommand(_ => RunDefenderScan());
+            RunMaintenanceCmd    = new RelayCommand(_ => RunMaintenance());
+            SmartCleanupCmd      = new RelayCommand(_ => RunSmartCleanup());
+            CleanBrowsersCmd     = new RelayCommand(_ => RunBrowsersCleanup());
+            UpdateAllCmd         = new RelayCommand(_ => RunUpdateAll());
+            RunDefenderScanCmd   = new RelayCommand(_ => RunDefenderScan());
             OpenConfigurationCmd = new RelayCommand(_ => OpenConfiguration());
         }
 
-        public ICommand RunMaintenanceCmd { get; }
-        public ICommand SmartCleanupCmd { get; }
-        public ICommand CleanBrowsersCmd { get; }
-        public ICommand UpdateAllCmd { get; }
-        public ICommand RunDefenderScanCmd { get; }
+        public ICommand RunMaintenanceCmd    { get; }
+        public ICommand SmartCleanupCmd      { get; }
+        public ICommand CleanBrowsersCmd     { get; }
+        public ICommand UpdateAllCmd         { get; }
+        public ICommand RunDefenderScanCmd   { get; }
         public ICommand OpenConfigurationCmd { get; }
-
-        private void AppendChat(string text)
-        {
-            if (string.IsNullOrWhiteSpace(text)) return;
-
-            _chat.PostSystemMessage(text);
-        }
     }
 }
