e2656ec5d8b7696d6c3fc2f4dfbda9ecc5fc5c8f
thoms <bassetthomas@hotmail.fr>
2025-12-26 12:54:33 +0100

    Add SystemMetricsModel class with property change notifications

---
+++ Virgil.Core/Models/SystemMetricsModel.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel;
+
+namespace Virgil.Core.Models
+{
+    public class SystemMetricsModel : INotifyPropertyChanged
+    {
+        private float _cpuUsage;
+        private float _ramUsage;
+        private float _cpuTemp;
+        private float _gpuTemp;
+        private float _diskUsage;
+
+        public float CpuUsage
+        {
+            get => _cpuUsage;
+            set { _cpuUsage = value; OnPropertyChanged(nameof(CpuUsage)); }
+        }
+
+        public float RamUsage
+        {
+            get => _ramUsage;
+            set { _ramUsage = value; OnPropertyChanged(nameof(RamUsage)); }
+        }
+
+        public float CpuTemp
+        {
+            get => _cpuTemp;
+            set { _cpuTemp = value; OnPropertyChanged(nameof(CpuTemp)); }
+        }
+
+        public float GpuTemp
+        {
+            get => _gpuTemp;
+            set { _gpuTemp = value; OnPropertyChanged(nameof(GpuTemp)); }
+        }
+
+        public float DiskUsage
+        {
+            get => _diskUsage;
+            set { _diskUsage = value; OnPropertyChanged(nameof(DiskUsage)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
