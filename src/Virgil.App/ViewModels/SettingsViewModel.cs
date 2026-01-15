91409140768d982d79112955218e990b4fb21f88
thoms <bassetthomas@hotmail.fr>
2026-01-15 11:26:31 +0100

    Fix SettingsViewModel build by restoring StringBuilder using

--- src/Virgil.App/ViewModels/SettingsViewModel.cs
+++ src/Virgil.App/ViewModels/SettingsViewModel.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
